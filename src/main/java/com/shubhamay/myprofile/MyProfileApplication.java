08a814c4a58b72a60c58936badaa6f3e2b2618f8
Shubhamay <shubhamaydas1@gmail.com>
2023-01-15 14:17:18 +0100

    initial commit

---
+++ src/main/java/com/shubhamay/myprofile/MyProfileApplication.java
@@ -0,0 +1,13 @@
+package com.shubhamay.myprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubhamay/myprofile/MyProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhamay.myprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
